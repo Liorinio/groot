df2ac01b2387e78eacb10f27d89799bbaa6627c8
Lior Koren <im.lior.koren@gmail.com>
2026-03-10 15:36:06 +0200

    adding docstring to the Task class

--- task.py
+++ task.py
@@ -1,19 +1,21 @@
-from logging import exception
 from typing import Any
 
 
 class Task:
-    def __init__(self, task_id:int, max_retries: int, name: str, exceptions_retry:dict[exception,bool]):
+    def __init__(self, task_id:int, max_retries: int, name: str, exceptions_retry:dict[Exception,bool]):
         self.task_id = task_id
         self.max_retries = max_retries
         self.name = name
-        self.exceptions_retry = exceptions_retry
+        self.exceptions_retry = exceptions_retry # a dict that contains exceptions and if the user wants to use them or not
 
+    # the function that the user overrides in order to implement his code
     def action(self, user_input: Any|None):
         pass
 
-    def retry(self, required_exception:exception, is_on:bool):
+    # gets an exception and runs the action again
+    def retry(self, required_exception:Exception, is_on:bool):
         ...
 
+    # runs on the exceptions array, checks which exception is on and runs the retry function
     def on_failure(self):
         ...
