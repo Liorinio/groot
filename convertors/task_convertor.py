90ecfbaa114786849d007eb2f9cdabce02b0f96e
נועה גוהר <noagohar88@gmail.com>
2026-03-14 18:30:24 +0200

    added the correct constructor to taskconverotr

--- convertors/task_convertor.py
+++ convertors/task_convertor.py
@@ -5,19 +5,21 @@ from convertors.convertors import AirflowConvertor
 
 class AirflowTaskConvertor(AirflowConvertor):
 
-    def __init__(self, task: Task):
+    def __init__(self, task: Task, dag_id: str, depended_task_id: str):
         """
         The AirflowTaskConvertor's constructor.
         It receives a Task
         """
         self.task = task
+        self.dag_id = dag_id
+        self.depended_task_id = depended_task_id
 
     def convert(self) -> PythonOperator:
         """
         The function creates a python operator from the convert_with_input() function
         """
 
-    def convert_with_input(self, **context):
+    def wrapped_action(self, **context):
         """
         The function wraps the clients task with the s3 writer and reader
         """
