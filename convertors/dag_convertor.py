38bd1b8dda66554300b07f0f0cb1e21b8d131a1b
danit <danit.sigal@gmail.com>
2026-03-14 13:21:10 +0000

    split  convert pytasks to airfkow tasks to smaller functions

--- convertors/dag_convertor.py
+++ convertors/dag_convertor.py
@@ -15,66 +15,48 @@ class AirflowDagConverter(AirflowConvertor):
         """
         self.dag = dag
 
-    def convert(self):
-        created_dag = DAG(
-            dag_id= self.dag.dag_id
-            start_date= self.dag.
-        )
-    
+    def _get_task_by_id(tasks:dict[PythonOperator, list[PythonOperator]],task_id:str) ->PythonOperator:
+        for task in tasks.keys:
+            if task.task_id == task_id:
+                return task
+            
+    def _convert_single_pytask_to_airflow_tasks(self,tasks_id_set:set,task_id:str,
+                                                tasks:dict[PythonOperator, list[PythonOperator]])->PythonOperator:
+         task = AirflowTaskConvertor(self._get_task_by_id(tasks,task_id)).convert()
+         tasks_id_set.add(task_id)
+         return task
+
+    def _convert_depend_tasks(self,python_tasks:list[Task],tasks_id_set:set,
+                              dag_tasks:dict[PythonOperator,list[PythonOperator]])->list[PythonOperator]:
         
-
-
-        return created_dag
-    
-
-
-    def _convert_first_pytask_to_airflow_task(self) -> PythonOperator:
-        task = self._find_first_task
-        convertor = AirflowTaskConvertor(task)
-        converted_task = convertor.convert(None)
-        return converted_task
+        depend_tasks:list[PythonOperator] =[]
+        for depend_task in python_tasks:
+                if depend_task.task_id not in tasks_id_set:
+                    converted_depend_task = self._convert_single_pytask_to_airflow_tasks(tasks_id_set,depend_task.task_id,dag_tasks)
+                    depend_tasks.append(converted_depend_task)
+                    dag_tasks[converted_depend_task] = []
+                else:
+                    tasks_id_set.add(depend_task.task_id)
+        return depend_tasks
 
     def _convert_pytasks_to_airflow_tasks(self) -> dict[PythonOperator, list[PythonOperator]]:
-        first_task:PythonOperator = self._convert_first_pytask_to_airflow_task()
-        set_airflow_tasks:set ={first_task.dag_id}
-        airflow_tasks: dict[PythonOperator, list[PythonOperator]] = {first_task:[]}
+        tasks_id_set:set =set()
+        airflow_tasks: dict[PythonOperator, list[PythonOperator]] = {}
         tasks = self.dag.tasks
         
 
         for key in tasks.keys():
             depend_tasks:list[PythonOperator] =[]
-            if key.task_id not in set_airflow_tasks:
-                task = AirflowTaskConvertor(self._get_task_by_id(key.task_id)).convert()
-                set_airflow_tasks.add(task.task_id)
+            if key.task_id not in tasks_id_set:
+                task = self._convert_single_pytask_to_airflow_tasks(key.task_id,airflow_tasks,tasks_id_set)
                 airflow_tasks[task] = []
-            for depend_task in tasks.get(self._get_task_by_id(key.task_id)):
-                if depend_task.task_id not in set_airflow_tasks:
-                    converted_depend_task = AirflowTaskConvertor(self._get_task_by_id
-                                                                 (depend_task.task_id)).convert()
-                    set_airflow_tasks.add(converted_depend_task.task_id)
-                    depend_tasks.append(converted_depend_task)
-                    airflow_tasks[converted_depend_task] = []
-                else:
-                    set_airflow_tasks.add(depend_task.task_id)
+            depend_tasks = self._convert_depend_tasks(tasks.get(key),tasks_id_set,airflow_tasks)
             airflow_tasks[self._get_task_by_id(key.task_id)] = depend_tasks
-
-
-
-
-               
-
-
-
-
-
-    def _get_task_by_id(tasks:dict[PythonOperator, list[PythonOperator]],task_id:str) ->PythonOperator:
-        for task in tasks.keys:
-            if task.task_id == task_id:
-                return task
-
-
-    def set_task_dependencies(self,task: PythonOperator):
-        task.set_downstream
+        return airflow_tasks
+            
+    def _set_dependecies_for_tasks(tasks:dict[PythonOperator, list[PythonOperator]]):
+        for task in tasks.keys():
+            task.set_downstream(tasks.get(task))
 
     def _find_first_task(self) -> Task:
         """
@@ -98,3 +80,14 @@ class AirflowDagConverter(AirflowConvertor):
         a function that checks if the dag is a validate one
         """
         ...
+
+    def convert(self):
+        created_dag = DAG(
+            dag_id= self.dag.dag_id
+            start_date= self.dag.
+        )
+    
+        
+
+
+        return created_dag
