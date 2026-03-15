72125b2a0b9c842ebde60c2564b1922ce97e7c52
tito1233 <titotv1010@gmail.com>
2026-03-15 12:46:17 +0200

    fix merge

--- basics/model_task.py
+++ basics/model_task.py
@@ -20,15 +20,10 @@ def process(input_data,model):
         print(f"An error occurred during model loading or inference: {e}")
         return None
 
+
 class DefaultModelTask(Task):
-    def action(self, user_input: Any | None):
-        self.__load_model()
-        process(user_input, self.model)
 
-    def on_failure(self) -> Callable:
-        return self.__check_storage()
-
-    def __init__(self,task_id: str,max_retries: int,name: str, exceptions_retry: dict[Exception, bool],model_path: str):
+    def __init__(self,task_id: str,max_retries: int,name: str, exceptions_retry: dict[Exception, bool], model_path: str):
         super().__init__(task_id, max_retries, name, exceptions_retry)
 
         """
@@ -37,6 +32,13 @@ class DefaultModelTask(Task):
 
         self.model_path = model_path
         self.model = None
+
+    def action(self, user_input: Any | None):
+        self.__load_model()
+        process(user_input, self.model)
+
+    def on_failure(self) -> Callable:
+        return self.__check_storage()
 
     def __load_model(self):
         """
@@ -52,14 +54,14 @@ class DefaultModelTask(Task):
             return None
 
     def __check_storage(self):
-        if FileNotFoundError in self.exceptions_retry.keys() and self.exceptions_retry[FileNotFoundError()]:
+        if FileNotFoundError in self.exceptions_retry.keys() and self.exceptions_retry[FileNotFoundError]:
             print("Check if you saved the model and if you did, check where did you saved it")
         else:
-            self.exceptions_retry[FileNotFoundError()] = True
+            self.exceptions_retry[FileNotFoundError] = True
 
 
 @app.post("/predictions")
-def get_predictions(user_input: Any | None, task_id: str,max_retries: int,name: str, exceptions_retry: dict[Exception, bool],model_path: str):
+def get_predictions(user_input: Any | None, task_id: str, max_retries: int, name: str, exceptions_retry: dict[Exception, bool], model_path: str):
     model_task = DefaultModelTask(task_id,max_retries ,name, exceptions_retry,model_path)
     deploy_uvicorn()
     return model_task.action(user_input)
