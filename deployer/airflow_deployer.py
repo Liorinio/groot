d15fad98ac75d36fe12902a4b4d7bea87d9135c1
נועה גוהר <noagohar88@gmail.com>
2026-03-15 18:20:58 +0200

    added the convertaion from a dag object ti a dag file

--- deployer/airflow_deployer.py
+++ deployer/airflow_deployer.py
@@ -1,3 +1,5 @@
+import pickle
+from airflow.sdk import DAG
 from deployer.deployer import Deployer
 import os
 import shutil
@@ -6,18 +8,18 @@ import subprocess
 
 class AirflowDeployer(Deployer):
 
-    def __init__(self, dag_file_path: str, repo_url: str, dags_dir: str = "/opt/airflow/dags"):
+    def __init__(self, dag_object: DAG, repo_url: str, dags_dir: str = "/opt/airflow/created_dags"):
         """
-        The '__init__()' function gets a dag file path, a remote repo URL, and a local dags directory
+        The '__init__()' function gets a dag file path, a remote repo URL, and a local created_dags directory
         and initializes the AirflowDeployer.
         """
-        self.dag_file_path = dag_file_path
+        self.dag_object = dag_object
         self.repo_url = repo_url
         self.dags_dir = dags_dir
 
     def _git_sync(self) -> bool:
         """
-        The '_git_sync()' function syncs the local dags directory with the remote git repository
+        The '_git_sync()' function syncs the local created_dags directory with the remote git repository
         clones it if it doesn't exist, else it pulls the latest changes, and returns True on success.
         """
         try:
@@ -51,16 +53,36 @@ class AirflowDeployer(Deployer):
             print(f"Git push failed: {e}")
             return False
 
+    def create_dag_file(self) -> str:
+        pickle_dir_path = os.path.abspath("pickles")
+        dags_dir_path = os.path.abspath("created_dags")
+        pickle_file_path = f"{pickle_dir_path}/dag_{self.dag_object.dag_id}.pickle"
+
+        with open(f"{pickle_file_path}", "wb") as file:
+            pickle.dump(self.dag_object, file)
+
+        dag_file_path = f"{dags_dir_path}/{self.dag_object.dag_id}.py"
+        template_file_path = f"{dags_dir_path}/template_dag.py"
+        shutil.copy(template_file_path, dag_file_path)
+
+        return dag_file_path
+
+
+
     def deploy(self) -> bool:
         """
-        The 'deploy()' function syncs the dags directory with git and copies the dag file into it,
+        The 'deploy()' function syncs the created_dags directory with git and copies the dag file into it,
         so that Airflow's Executor can detect and run it.
         """
+
+        dag_file_path = self.create_dag_file()
+
+        if not self.push_dag_to_git(dag_file_path):
+            return False
+
         if not self._git_sync():
             return False
 
-        if not self.push_dag_to_git(self.dag_file_path):
-            return False
 
-        print(f"DAG '{os.path.basename(self.dag_file_path)}' deployed successfully.")
+        print(f"DAG '{os.path.basename(dag_file_path)}' deployed successfully.")
         return True
